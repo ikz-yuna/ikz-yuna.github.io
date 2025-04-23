eea13b09ebfec64478f6dd955a2939f2aa6c7b7f
_ikz <naaa_1001@qq.com>
2025-04-23 20:53:00 +0800

    Update footer.js

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,7 +1,7 @@
 document.write('<!-----------  footer----------->');
 document.write('' +
     '<center>\n' +
-    '<p style="color:#333;">© 2025 YunaHome</p>\n' +
+    '<p style="color:#333;">© 2024-2025 YunaHome</p>\n' +
     '</center>\n' +
     '');
 
